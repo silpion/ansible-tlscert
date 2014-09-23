47d537ad9a12a9b06c34097b55b4eebc658398f6
Mark Kusch <mark.kusch@gmail.com>
2014-09-23 18:04:51 +0200

    Fix local facts destination

--- spec/default/ssl-certificates_spec.rb
+++ spec/default/ssl-certificates_spec.rb
@@ -50,3 +50,25 @@ describe file('/etc/ssl/cnf/foo.example.com-openssl.cnf') do
     its(:content) { should match line }
   end
 end
+
+
+describe file('/etc/ansible/facts.d') do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 755 }
+end
+
+describe file('/etc/ansible/facts.d/ssl-certificates.fact') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 644 }
+  %w{
+    ssl_certificates_cnf_path=/etc/ssl/cnf
+    ssl_certificates_crt_path=/etc/ssl/certs
+    ssl_certificates_key_path=/etc/ssl/private
+  }.each do |line|
+    its(:content) { should match line }
+  end
+end
